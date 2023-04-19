4533623285b136ff98e550d8d2ea759b227dcf4b
rozkocha <aaaa>
2023-04-19 18:17:14 +0200

    finish

--- js/main.js
+++ js/main.js
@@ -1,32 +1,10 @@
-setInterval(() => {
-  const game = document.querySelector("#game");
+const endGame = new EndGame();
 
-  const egg = new Image();
-  egg.addEventListener("load", () => {
-    egg.classList.add("egg");
-    const x = Math.round(Math.random() * (120 - (5 * egg.width / egg.height)));
-    egg.style.left = x + "vh";
-    game.appendChild(egg);
-  });
+const game = new Game(endGame);
 
-  egg.src = "images/easter-653985_640.png";
-}, 1000);
+endGame.game = game;
 
-setInterval(() => {
-  document.querySelectorAll("#game > .egg").forEach(element => {
-    const top = element.style.top.length !== 0 ? parseFloat(element.style.top) : 0;
-    console.log(element.style.top);
-    element.style.top = (top + 0.3) + "vh";
-
-    if(top > 100) {
-      element.remove();
-
-      const heart = document.querySelector(".heart");
-      heart.remove();
-      if(!document.querySelector(".heart")) {
-        alert("Game Over!");
-        window.location.reload();
-      }
-    }
-  })
-}, 30 / 1000);
+document.querySelector("#start-game > button").addEventListener("click", () => {
+  document.querySelector("#start-game").style.display = "none";
+  game.start();
+})
